f331d4a92b91f14478b8abb4ce7e942b023d345a
Ricardo Ferraz Leal <ricardo.leal@ill.eu>
2014-02-26 17:14:08 +0100

    test handler for queries specs

--- src/handlers/query/testDefinition.py
+++ src/handlers/query/testDefinition.py
@@ -17,11 +17,11 @@ class Test(unittest.TestCase):
         self.assertTrue(q.doesRemoteFunctionExist("theta_vs_counts"))
         self.assertFalse(q.doesLocalFunctionExist("theta_vs_counts1"))
         self.assertFalse(q.doesRemoteFunctionExist("theta_vs_counts1"))
-        self.assertEqual(q.getExecutableFullPath("theta_vs_counts"),"/home/leal/git/reductionServer/scripts/theta_vs_counts_IN5.sh")
-        self.assertEqual(q.getExecutableTimeout("theta_vs_counts"),30)
-        
+        self.assertEqual(q.getExecutableFullPath("theta_vs_counts"), 
+                         "/home/leal/git/reductionServer/scripts/theta_vs_counts_IN5.sh")
+        self.assertEqual(q.getExecutableTimeout("theta_vs_counts"), 30)
 
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testQuery']
+    # import sys;sys.argv = ['', 'Test.testQuery']
     unittest.main()
